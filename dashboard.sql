9544ad8de9094cf3382a2d45ed8a091b45b8c12b
lebron <solarhdr3@duck.com>
2026-05-12 02:27:10 +0300

    modify dashboard 2 corrected

--- dashboard.sql
+++ dashboard.sql
@@ -1,4 +1,3 @@
-/*
 WITH buffer AS (
     SELECT
         s.visitor_id,
@@ -12,198 +11,163 @@ WITH buffer AS (
         l.closing_reason,
         l.status_id,
         ROW_NUMBER() OVER (
-          PARTITION BY s.visitor_id
-          ORDER BY s.visit_date DESC
+            PARTITION BY s.visitor_id
+            ORDER BY s.visit_date DESC
         ) AS rn
     FROM sessions AS s
     LEFT JOIN leads AS l
         ON s.visitor_id = l.visitor_id AND s.visit_date <= l.created_at
     WHERE
-        s.medium <> 'organic'
-    )
-  SELECT
-    utm_source,
-    COUNT(DISTINCT visitor_id) AS visitors_count,
-    COUNT(DISTINCT CASE WHEN NOT lead_id IS NULL
-      THEN visitor_id END) AS leads_count,
-    COUNT(DISTINCT CASE WHEN status_id = 142
-      THEN visitor_id END) AS purchases_count,
-    SUM(CASE WHEN NOT lead_id IS NULL THEN amount ELSE 0 END) AS revenue
-  FROM buffer
-  WHERE
-    rn = 1
-  GROUP BY
-    utm_source
-  ORDER BY
-    visitors_count DESC
+        s.medium != 'organic'
+),
 
 aggr_last AS (
     SELECT
-      b.utm_source,
-      b.utm_medium,
-      b.utm_campaign,
-      CAST(b.visit_date AS DATE) AS visit_date,
-      COUNT(b.visitor_id) AS visitors_count,
-      COUNT(b.lead_id) AS leads_count,
-      COUNT(CASE WHEN b.status_id = 142
-        THEN b.visitor_id END) AS purchases_count,
-      SUM(CASE WHEN b.status_id = 142 THEN b.amount END) AS revenue
+        b.utm_source,
+        b.utm_medium,
+        b.utm_campaign,
+        CAST(b.visit_date AS DATE) AS visit_date,
+        COUNT(b.visitor_id) AS visitors_count,
+        COUNT(b.lead_id) AS leads_count,
+        COUNT(CASE
+            WHEN b.status_id = 142
+                THEN b.visitor_id
+        END) AS purchases_count,
+        SUM(CASE
+            WHEN b.status_id = 142
+                THEN b.amount
+        END) AS revenue
     FROM buffer AS b
     WHERE
-      b.rn = 1
+        b.rn = 1
     GROUP BY
-      b.utm_source,
-      b.utm_medium,
-      b.utm_campaign,
-      CAST(b.visit_date AS DATE)
-  ), ads_costs AS (
+        b.utm_source,
+        b.utm_medium,
+        b.utm_campaign,
+        CAST(b.visit_date AS DATE)
+),
+
+ads_costs AS (
     SELECT
-      CAST(va.campaign_date AS DATE) AS campaign_date,
-      va.utm_source,
-      va.utm_medium,
-      va.utm_campaign,
-      SUM(va.daily_spent) AS total_cost
+        CAST(va.campaign_date AS DATE) AS campaign_date,
+        va.utm_source,
+        va.utm_medium,
+        va.utm_campaign,
+        SUM(va.daily_spent) AS total_cost
     FROM vk_ads AS va
     GROUP BY
-      CAST(va.campaign_date AS DATE),
-      va.utm_source,
-      va.utm_medium,
-      va.utm_campaign
+        CAST(va.campaign_date AS DATE),
+        va.utm_source,
+        va.utm_medium,
+        va.utm_campaign
     UNION
     SELECT
-      CAST(ya.campaign_date AS DATE) AS campaign_date,
-      ya.utm_source,
-      ya.utm_medium,
-      ya.utm_campaign,
-      SUM(ya.daily_spent) AS total_cost
+        CAST(ya.campaign_date AS DATE) AS campaign_date,
+        ya.utm_source,
+        ya.utm_medium,
+        ya.utm_campaign,
+        SUM(ya.daily_spent) AS total_cost
     FROM ya_ads AS ya
     GROUP BY
-      CAST(ya.campaign_date AS DATE),
-      ya.utm_source,
-      ya.utm_medium,
-      ya.utm_campaign
-  ), final AS (
-    SELECT
-      al.visit_date,
-      al.visitors_count,
-      al.utm_source,
-      al.utm_medium,
-      al.utm_campaign,
-      ac.total_cost,
-      al.leads_count,
-      al.purchases_count,
-      al.revenue
-    FROM aggr_last AS al
+        CAST(ya.campaign_date AS DATE),
+        ya.utm_source,
+        ya.utm_medium,
+        ya.utm_campaign
+),
 
+final AS (
+    SELECT
+        al.visit_date,
+        al.visitors_count,
+        al.utm_source,
+        al.utm_medium,
+        al.utm_campaign,
+        ac.total_cost,
+        al.leads_count,
+        al.purchases_count,
+        al.revenue
+    FROM aggr_last AS al
     LEFT JOIN ads_costs AS ac
-      ON al.visit_date = ac.campaign_date
-      AND al.utm_medium = ac.utm_medium
-      AND al.utm_campaign = ac.utm_campaign
-      AND al.utm_source = ac.utm_source
+        ON
+            al.visit_date = ac.campaign_date
+            AND al.utm_medium = ac.utm_medium
+            AND al.utm_campaign = ac.utm_campaign
+            AND al.utm_source = ac.utm_source
     ORDER BY
-      al.revenue DESC NULLS LAST,
-      al.visit_date ASC,
-      al.visitors_count DESC,
-      al.utm_source ASC,
-      al.utm_medium ASC
-  )
-  SELECT
+        al.revenue DESC NULLS LAST,
+        al.visit_date ASC,
+        al.visitors_count DESC,
+        al.utm_source ASC,
+        al.utm_medium ASC
+)
+
+SELECT
     utm_source AS source,
-    SUM(total_cost) AS costs,
+    SUM(total_cost) AS ads_costs,
     SUM(revenue) AS revenue
-  FROM final
-  WHERE
+FROM final
+WHERE
     utm_source = 'vk' OR utm_source = 'yandex'
-  GROUP BY
+GROUP BY
     utm_source
 
-    SELECT
+SELECT
     EXTRACT(DAY FROM visit_date) AS date,
     COUNT(DISTINCT visitor_id) AS visitors_count,
     COUNT(DISTINCT CASE WHEN NOT lead_id IS NULL
-      THEN visitor_id END) AS leads_count,
+        THEN visitor_id END) AS leads_count,
     COUNT(DISTINCT CASE WHEN status_id = 142
-      THEN visitor_id END) AS purchases_count
-  FROM buffer
-  WHERE
+        THEN visitor_id END) AS purchases_count
+FROM buffer
+WHERE
     rn = 1
-  GROUP BY
-    date
-  ORDER BY
-    date ASC
+GROUP BY
+    EXTRACT(DAY FROM visit_date)
+ORDER BY
+    EXTRACT(DAY FROM visit_date) ASC
 
-    SELECT
-  DATE_TRUNC('DAY', campaign_date) AS campaign_date,
-  utm_source AS utm_source,
-  MAX(total_cost) AS "MAX(total_cost)"
+SELECT
+    DATE_TRUNC('DAY', campaign_date) AS campaign_date,
+    utm_source AS utm_source,
+    MAX(total_cost) AS "MAX(total_cost)"
 FROM (
-  SELECT
+    SELECT
     CAST(campaign_date AS DATE) AS campaign_date,
     utm_source,
     SUM(daily_spent) AS total_cost
-  FROM (
+FROM (
     SELECT
-      campaign_date,
-      utm_source,
-      utm_medium,
-      utm_campaign,
-      daily_spent
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        daily_spent
     FROM vk_ads
     UNION ALL
     SELECT
-      campaign_date,
-      utm_source,
-      utm_medium,
-      utm_campaign,
-      daily_spent
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        daily_spent
     FROM ya_ads
-  ) AS combined_ads
-  GROUP BY
+)) AS combined_ads
+GROUP BY
     campaign_date,
     utm_source
-  ORDER BY
-    campaign_date
-) AS virtual_table
-WHERE
-  TRUE
-GROUP BY
-  DATE_TRUNC('DAY', campaign_date),
-  utm_source
 ORDER BY
-  "MAX(total_cost)" DESC
+    campaign_date
 
-  final AS (
-    SELECT
-      al.visit_date,
-      al.visitors_count,
-      al.utm_source,
-      al.utm_medium,
-      al.utm_campaign,
-      ac.total_cost,
-      al.leads_count,
-      al.purchases_count,
-      al.revenue
-    FROM aggr_last AS al
-    LEFT JOIN ads_costs AS ac
-      ON al.visit_date = ac.campaign_date
-      AND al.utm_medium = ac.utm_medium
-      AND al.utm_campaign = ac.utm_campaign
-      AND al.utm_source = ac.utm_source
-    ORDER BY
-      al.revenue DESC NULLS LAST,
-      al.visit_date ASC,
-      al.visitors_count DESC,
-      al.utm_source ASC,
-      al.utm_medium ASC
-  )
-  SELECT
+SELECT
+    utm_source AS source,
     ROUND(SUM(total_cost) / SUM(visitors_count), 2) AS cost_per_click,
     ROUND(SUM(total_cost) / SUM(leads_count), 2) AS cost_per_lead,
     ROUND(SUM(total_cost) / SUM(purchases_count), 2) AS cost_per_customer,
     ROUND((
-      SUM(revenue) - SUM(total_cost)
+        SUM(revenue) - SUM(total_cost)
     ) / SUM(total_cost) * 100, 2) AS ROI
-  FROM final
-  HAVING
+FROM final
+GROUP BY
+    utm_source
+HAVING
     NOT ROUND(SUM(total_cost) / SUM(visitors_count), 2) IS NULL
-*/
